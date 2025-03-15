e13df6ee4d05d3d8e7e081af14a4ab585462064b
Ry02024 <162527496+Ry02024@users.noreply.github.com>
2025-03-16 00:46:37 +0900

    config.py の更新

--- app/config.py
+++ app/config.py
@@ -1,5 +1,7 @@
 import os
 
 class Config:
-    SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN", "your-slack-bot-token")
-    SLACK_TEST_CHANNEL =["C08BRQGQ2VB"]
+    # Bot Token は環境変数から取得（必須）
+    SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN", "").split()
+    # チャンネルはカンマ区切りの文字列として環境変数に設定し、リストに変換する
+    SLACK_TEST_CHANNELS = ["C08BRQGQ2VB"]
