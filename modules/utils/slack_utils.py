f16a525c4dc8a2636081d6aa6f04ace97f3950e1
Ry02024 <162527496+Ry02024@users.noreply.github.com>
2025-03-16 00:06:20 +0900

    slack_utils.py の更新

--- modules/utils/slack_utils.py
+++ modules/utils/slack_utils.py
@@ -4,7 +4,7 @@ import requests
 def post_to_slack(message, channel):
 #    SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN", "").strip()
 #    if not SLACK_BOT_TOKEN:
-        print("Error: SLACK_BOT_TOKEN is not set")
+#        print("Error: SLACK_BOT_TOKEN is not set")
 #        return
 
     # デバッグ用: トークンの先頭と末尾数文字を表示（※本番では削除してください）
