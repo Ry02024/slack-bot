80d27a26cd1549418facd1a2d44777fccea999ef
Ry02024 <162527496+Ry02024@users.noreply.github.com>
2025-03-15 12:24:38 +0900

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN", "your-slack-bot-token")
-    SLACK_TEST_CHANNEL = os.getenv("SLACK_TEST_CHANNEL", "your-slack-test-channel-id")
+    SLACK_TEST_CHANNEL =["C08BRQGQ2VB"]
